66b9cbf87921cfe0068a7ca9b44e389c8be2cca0
fbennett <biercenator@gmail.com>
2016-12-29 11:13:41 +0900

    WIP checkin.

--- build.sh
+++ build.sh
@@ -8,7 +8,7 @@ set -e
 PRODUCT="Juris-M Abbreviation Filter: abbreviation list editing and maintenance"
 IS_BETA="false"
 FORK="abbrevs-filter"
-BRANCH="af-5.0"
+BRANCH="af5"
 CLIENT="abbrevs-filter"
 VERSION_ROOT="1.1."
 SIGNED_STUB="juris_m_abbreviation_filter-"
